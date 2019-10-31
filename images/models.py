99ad54ed687cb1bc06044e473541cff624147e70
sadakchap <aliceprerna@gmail.com>
2019-10-31 18:34:51 +0530

    Basic... :)

--- images/models.py
+++ images/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+    image           = models.ImageField()
+    desc            = models.TextField(max_length=255, blank=True)
+    uploaded_at     = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-uploaded_at',)
+        verbose_name = 'Photo' 
+        verbose_name_plural = 'Photos'
